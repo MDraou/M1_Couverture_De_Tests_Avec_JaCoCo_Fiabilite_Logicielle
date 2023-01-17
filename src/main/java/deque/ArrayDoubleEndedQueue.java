3efcc3ff24d0a81533610a8615fd144289cc4b60
ARTAUD Cyril <cyril.artaud@etu.univ-amu.fr>
2023-01-17 17:55:53 +0100

    ajout des methodes de la classe

--- src/main/java/deque/ArrayDoubleEndedQueue.java
+++ src/main/java/deque/ArrayDoubleEndedQueue.java
@@ -4,43 +4,74 @@ public class ArrayDoubleEndedQueue<E> implements DoubleEndedQueue<E> {
 
     private Object[] queue;
 
+    private int indexFirst = 0;
+
+    private int capacity;
+
+    private int size = 0;
+
+    public ArrayDoubleEndedQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
     @Override
     public void addFirst(E e) {
-
+        if (size != capacity) {
+            queue[indexFirst] = e;
+            indexFirst = (indexFirst + capacity - 1) % capacity;
+            size++;
+        }
     }
 
     @Override
     public void addLast(E e) {
-
+        if (size != capacity) {
+            queue[(indexFirst + size) % capacity] = e;
+            size++;
+        }
     }
 
     @Override
     public E removeFirst() {
+        if (size > 0) {
+            E element = (E) queue[indexFirst];
+            indexFirst = (indexFirst + 1) % capacity;
+            size--;
+            return element;
+        }
         return null;
     }
 
     @Override
     public E removeLast() {
+        if (size > 0) {
+            E element = (E) queue[(indexFirst + size-1) % capacity];
+            size--;
+            return element;
+        }
         return null;
     }
 
     @Override
     public E getFirst() {
-        return null;
+        return (E) queue[indexFirst];
     }
 
     @Override
     public E getLast() {
-        return null;
+        return (E) queue[(indexFirst + size - 1) % capacity];
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean contains(Object o) {
+        for (int i = 0 ; i < capacity ; i++) {
+            if (queue[i].equals(o)) return true;
+        }
         return false;
     }
 

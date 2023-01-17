c8683f0bb6055380d215000121a7eaf631d8ca96
draou <meryem.draou@etu.univ-amu.fr>
2023-01-17 17:01:04 +0100

    création de la classe ArrayDoubleEndedQueue

---
+++ src/main/java/deque/ArrayDoubleEndedQueue.java
@@ -0,0 +1,44 @@
+package deque;
+
+public class ArrayDoubleEndedQueue implements DoubleEndedQueue {
+
+    @Override
+    public void addFirst(Object o) {
+
+    }
+
+    @Override
+    public void addLast(Object o) {
+
+    }
+
+    @Override
+    public Object removeFirst() {
+        return null;
+    }
+
+    @Override
+    public Object removeLast() {
+        return null;
+    }
+
+    @Override
+    public Object getFirst() {
+        return null;
+    }
+
+    @Override
+    public Object getLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+}
